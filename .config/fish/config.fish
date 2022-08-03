2ecb63bb88ed1f4a575d9ef4bd87c4fc8eee5885
Tye Peck <tye@novatasolutions.com.au>
2022-08-03 11:55:04 +0800

    fix(fish): Remove zoxide plugin and configure manually

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,8 @@
 set fish_greeting # disable fish greeting
 set -Ux EDITOR nvim
 if status is-interactive
-# Commands to run in interactive sessions can go here
+  # Commands to run in interactive sessions can go here
+  zoxide init fish | source
 end
 
 set -Ux LS_COLORS "di=34:ln=35:so=32:pi=33:ex=37:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
--- .config/fish/functions/__zoxide_zi.fish
+++
@@ -1,4 +0,0 @@
-function __zoxide_zi
-    set -l result (command zoxide query -i -- $argv)
-    and __zoxide_cd $result
-end
