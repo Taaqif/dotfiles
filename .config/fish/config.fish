539310c33690f5a1c9c64c4908bc2bd40eeb03e5
Tye Peck <tye@novatasolutions.com.au>
2022-08-05 11:02:17 +0800

    fix(fish): update nvm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,3 +9,5 @@ end
 set -Ux LS_COLORS "di=34:ln=35:so=32:pi=33:ex=37:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 
 
+
+test -s /home/tye/.nvm/nvm.fish; and source /home/tye/.nvm/nvm.fish
