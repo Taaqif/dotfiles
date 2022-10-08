274ca5aad5427924777c871be09bcbcf583fe718
Tye Peck <tye@novatasolutions.com.au>
2022-10-08 23:44:15 +0800

    feat(nvim): update bufferline styling

--- .config/nvim/lua/config/bufferline.lua
+++ .config/nvim/lua/config/bufferline.lua
@@ -6,28 +6,25 @@ end
 
 bufferline.setup({
 	options = {
-		numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-		close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-		right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-		left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
-		middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
-		buffer_close_icon = "",
-		modified_icon = "●",
-		close_icon = "",
-		left_trunc_marker = "",
-		right_trunc_marker = "",
-		max_name_length = 20,
-		max_prefix_length = 15, -- prefix used when a buffer is deduplicated
-		tab_size = 20,
+		separator_style = { "|", "|" },
+		indicator = {
+			style = "none",
+		},
 		offsets = { { filetype = "NvimTree", text = "File Explorer" } },
+		diagnostics = "nvim_lsp",
+		diagnostics_indicator = function(count, level)
+			if level:match("error") then
+				local icon = " "
+				return " " .. icon .. count
+			else
+				return ""
+			end
+		end,
+		close_command = "Bdelete! %d",
+		right_mouse_command = "Bdelete! %d",
+		left_mouse_command = "buffer %d",
 		show_close_icon = false,
-		show_tab_indicators = true,
-		persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-		separator_style = "thin",
 		always_show_bufferline = true,
-		view = "multiwindow",
-		show_buffer_close_icons = true,
 		themable = true,
-		
 	},
 })
