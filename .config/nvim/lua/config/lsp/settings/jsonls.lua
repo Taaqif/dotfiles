73866fe25408ca1fa26eb2d4798a41915366aefd
Tye Peck <tye@novatasolutions.com.au>
2022-10-11 08:40:44 +0800

    fix(nvim): fix schemastore not loading

--- .config/nvim/lua/config/lsp/settings/jsonls.lua
+++ .config/nvim/lua/config/lsp/settings/jsonls.lua
@@ -10,15 +10,9 @@ return {
 	settings = {
 		json = {
 			schemas = schemastore.json.schemas(),
+			validate = { enable = true },
 		},
 	},
 	setup = {
-		commands = {
-			Format = {
-				function()
-					vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line("$"), 0 })
-				end,
-			},
-		},
 	},
 }
