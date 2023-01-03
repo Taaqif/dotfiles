bade8c36876a1d3e768e50e5497a03a2db5c1fd7
Tye Peck <tye@novatasolutions.com.au>
2023-01-03 12:40:13 +0800

    feat(nvim): use dial.nvim

---
+++ .config/nvim/lua/plugins/dial.lua
@@ -0,0 +1,24 @@
+return {
+  "monaqa/dial.nvim",
+  init = function()
+    vim.api.nvim_set_keymap("n", "<C-a>", require("dial.map").inc_normal(), { noremap = true })
+    vim.api.nvim_set_keymap("n", "<C-x>", require("dial.map").dec_normal(), { noremap = true })
+    vim.api.nvim_set_keymap("v", "<C-a>", require("dial.map").inc_visual(), { noremap = true })
+    vim.api.nvim_set_keymap("v", "<C-x>", require("dial.map").dec_visual(), { noremap = true })
+    vim.api.nvim_set_keymap("v", "g<C-a>", require("dial.map").inc_gvisual(), { noremap = true })
+    vim.api.nvim_set_keymap("v", "g<C-x>", require("dial.map").dec_gvisual(), { noremap = true })
+  end,
+  config = function()
+    local augend = require("dial.augend")
+    require("dial.config").augends:register_group({
+      default = {
+        augend.integer.alias.decimal_int,
+        augend.integer.alias.hex,
+        augend.constant.alias.bool,
+        augend.date.alias["%Y/%m/%d"],
+        augend.constant.alias.alpha,
+        augend.constant.alias.Alpha,
+      },
+    })
+  end,
+}
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -9,7 +9,6 @@ return {
 	{"MunifTanjim/nui.nvim", event = "VeryLazy"},
 	{ "stevearc/dressing.nvim", event = "VeryLazy" },
 	{"tpope/vim-repeat", event = "VeryLazy"},
-	{"AndrewRadev/switch.vim", event = "VeryLazy"},
 	{"moll/vim-bbye", event = "VeryLazy"},
 	{"andymass/vim-matchup", event = "BufRead"},
 	{"wellle/targets.vim", event = "BufRead"},
