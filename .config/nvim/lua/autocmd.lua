8127fc15d658e2623942aa316688d62b18983da9
Tye Peck <tye@novatasolutions.com.au>
2023-01-06 11:24:57 +0800

    feat(nvim): add some useful autocmds

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,6 +1,49 @@
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = {
+    "qf",
+    "help",
+    "man",
+    "lspinfo",
+    "spectre_panel",
+    "lir",
+    "DressingSelect",
+    "tsplayground",
+    "Markdown",
+  },
+  callback = function()
+    vim.cmd [[
+      nnoremap <silent> <buffer> q :close<CR> 
+      nnoremap <silent> <buffer> <esc> :close<CR> 
+      set nobuflisted 
+    ]]
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = { "" },
+  callback = function()
+    local buf_ft = vim.bo.filetype
+    if buf_ft == "" or buf_ft == nil then
+      vim.cmd [[
+      nnoremap <silent> <buffer> q :close<CR> 
+      nnoremap <silent> <buffer> <c-j> j<CR> 
+      nnoremap <silent> <buffer> <c-k> k<CR> 
+      set nobuflisted 
+    ]]
+    end
+  end,
+})
+
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
   callback = function()
     vim.cmd("set formatoptions-=cro")
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
   end,
 })
 
