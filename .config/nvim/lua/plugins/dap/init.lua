c6163ba520b25d1303856680ade9d61a25d63ec9
Tye Peck <tye@novatasolutions.com.au>
2023-01-09 11:35:43 +0800

    feat(nvim): enhanced dap ui icons

--- .config/nvim/lua/plugins/dap/init.lua
+++ .config/nvim/lua/plugins/dap/init.lua
@@ -1,10 +1,57 @@
 return {
-  "mfussenegger/nvim-dap",
-  "rcarriga/nvim-dap-ui",
-  init = function ()
-    -- TODO: https://alpha2phi.medium.com/neovim-dap-enhanced-ebc730ff498b
-  end,
-  config = function ()
-    require("dapui").setup()
-  end
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+	},
+	init = function()
+		-- TODO: https://alpha2phi.medium.com/neovim-dap-enhanced-ebc730ff498b
+	end,
+	event = "VeryLazy",
+	config = function()
+		local dap, dapui = require("dap"), require("dapui")
+		local icons = require("icons")
+		local dap_breakpoint = {
+			breakpoint = {
+				text = icons.ui.BigUnfilledCircle,
+				texthl = "DiagnosticSignError",
+				linehl = "",
+				numhl = "",
+			},
+			breakpoint_rejected = {
+				text = icons.ui.BigUnfilledCircle,
+				texthl = "DiagnosticSignError",
+				linehl = "",
+				numhl = "",
+			},
+			stopped = {
+				text = icons.ui.BoldArrowRight,
+				texthl = "DiagnosticSignWarn",
+				linehl = "Visual",
+				numhl = "DiagnosticSignWarn",
+			},
+			log_point = {
+				text = icons.ui.ChevronRightDot,
+				texthl = "DiagnosticInfo",
+				linehl = "",
+				numhl = "",
+			},
+		}
+
+		vim.fn.sign_define("DapBreakpoint", dap_breakpoint.breakpoint)
+		vim.fn.sign_define("DapStopped", dap_breakpoint.stopped)
+		vim.fn.sign_define("DapBreakpointRejected", dap_breakpoint.breakpoint_rejected)
+		vim.fn.sign_define("DapLogPoint", dap_breakpoint.log_point)
+
+		dapui.setup({})
+
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated["dapui_config"] = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited["dapui_config"] = function()
+			dapui.close()
+		end
+	end,
 }
--- .config/nvim/lua/plugins/lsp/settings/rust_analyzer.lua
+++ .config/nvim/lua/plugins/lsp/settings/rust_analyzer.lua
@@ -1,18 +1,31 @@
 local rust_tools_ok, rust_tools = pcall(require, "rust-tools")
 local _, lspconfig = pcall(require, "lspconfig")
+local mason_registry_ok, mason_registry = pcall(require, "mason-registry")
 return {
-  setup = function(opts)
-    if rust_tools_ok then
-      rust_tools.setup({
-        server = opts,
-      })
-    else
-      lspconfig["rust_analyzer"].setup(opts)
-    end
-  end,
-  on_attach = function(client, bufnr)
-    vim.keymap.set("n", "<C-space>", rust_tools.hover_actions.hover_actions, { buffer = bufnr })
-    -- Code action groups
-    vim.keymap.set("n", "<Leader>lA", rust_tools.code_action_group.code_action_group, { buffer = bufnr, desc = "Rust code action" })
-  end,
+	setup = function(opts)
+		if rust_tools_ok and mason_registry_ok then
+			local codelldb = mason_registry.get_package("codelldb")
+			local extension_path = codelldb:get_install_path() .. "/extension/"
+			local codelldb_path = extension_path .. "adapter/codelldb"
+			local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+			rust_tools.setup({
+				server = opts,
+				dap = {
+					adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+				},
+			})
+		else
+			lspconfig["rust_analyzer"].setup(opts)
+		end
+	end,
+	on_attach = function(client, bufnr)
+		vim.keymap.set("n", "<C-space>", rust_tools.hover_actions.hover_actions, { buffer = bufnr })
+		-- Code action groups
+		vim.keymap.set(
+			"n",
+			"<Leader>lA",
+			rust_tools.code_action_group.code_action_group,
+			{ buffer = bufnr, desc = "Rust code action" }
+		)
+	end,
 }
