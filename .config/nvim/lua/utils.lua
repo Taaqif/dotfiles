b9ab16cf252a2a345391f01edfcc62663158c37b
Tye Peck <tye@novatasolutions.com.au>
2023-01-10 16:46:14 +0800

    perf(nvim): update keymap function

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -1,32 +1,29 @@
-local M = { }
+local M = {}
 
-function M.keymap(mode, lhs, rhs, desc)
-  if desc == nil or type(desc) == 'string' then 
-    vim.keymap.set(mode, lhs, rhs, {
-      noremap = true, 
-      silent = true,
-      desc = desc or nil
-    })
-  else
-    local opts = desc
-    if not opts.noremap then 
-      opts.noremap = true
-    end
-    if not opts.silent then 
-      opts.silent = true
-    end
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
+function M.keymap(mode, lhs, rhs, opts)
+	if opts == nil or type(opts) == "string" then
+		vim.keymap.set(mode, lhs, rhs, {
+			noremap = true,
+			silent = true,
+			desc = opts or nil,
+		})
+	else
+		local options = { noremap = true, silent = true }
+		if opts then
+			options = vim.tbl_extend("force", options, opts)
+		end
+		vim.keymap.set(mode, lhs, rhs, options)
+	end
 end
 
 function M.on_attach(on_attach)
-  vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(args)
-      local buffer = args.buf
-      local client = vim.lsp.get_client_by_id(args.data.client_id)
-      on_attach(client, buffer)
-    end,
-  })
+	vim.api.nvim_create_autocmd("LspAttach", {
+		callback = function(args)
+			local buffer = args.buf
+			local client = vim.lsp.get_client_by_id(args.data.client_id)
+			on_attach(client, buffer)
+		end,
+	})
 end
 
 return M
