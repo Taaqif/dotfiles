a5c7060d2cfb8df1168585429d26f4224948f152
Tye Peck <tye@novatasolutions.com.au>
2023-01-11 14:38:32 +0800

    feat(nvim): update typescript lsp config to include keymaps and null ls complete

--- .config/nvim/lua/plugins/lsp/null_ls.lua
+++ .config/nvim/lua/plugins/lsp/null_ls.lua
@@ -1,37 +1,44 @@
 local ok, null_ls = pcall(require, "null-ls")
 
 if not ok then
-  return
+	return
 end
 
 local completion = null_ls.builtins.completion
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
--- Fix dhis -> chis``
-null_ls.setup({
-  debug = false,
-  sources = {
 
-    -- diagnostics
-    diagnostics.cspell.with({
-      diagnostics_postprocess = function(diagnostic)
-        diagnostic.severity = vim.diagnostic.severity.HINT
-      end,
-    }),
-    -- formatting sources
-    formatting.stylua,
-    formatting.prettierd,
+local config = {
+	debug = false,
+	sources = {
 
-    -- hover sources
+		-- diagnostics
+		diagnostics.cspell.with({
+			diagnostics_postprocess = function(diagnostic)
+				diagnostic.severity = vim.diagnostic.severity.HINT
+			end,
+		}),
+		-- formatting sources
+		formatting.stylua,
+		formatting.prettierd,
 
-    -- completion sources
-    code_actions.cspell.with({
-      config = {
-        find_json = function(params)
-          return vim.fn.stdpath("config") .. "/cspell.json"
-        end,
-      },
-    }),
-  },
-})
+		-- hover sources
+
+		-- completion sources
+		code_actions.cspell.with({
+			config = {
+				find_json = function(params)
+					return vim.fn.stdpath("config") .. "/cspell.json"
+				end,
+			},
+		}),
+	},
+}
+
+local typescript_ok, typescript = pcall(require, "typescript.extensions.null-ls.code-actions")
+if typescript_ok then
+	table.insert(config.sources, typescript)
+end
+
+null_ls.setup(config)
