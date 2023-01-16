1416104fa2788e32b3b984a4cb91557361e81a1b
Tye Peck <tye@novatasolutions.com.au>
2023-01-16 14:56:44 +0800

    feat(nvim): bufferline slopes

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,39 +1,43 @@
 return {
-  "akinsho/bufferline.nvim",
-  event = "BufWinEnter",
-  config = function()
-    require("bufferline").setup({
-      options = {
-        separator_style = { "|", "|" },
-        indicator = {
-          style = "none",
-        },
-        offsets = { 
-          { filetype = "NvimTree", text = "File Explorer" },
-          {
-            filetype = "neo-tree",
-            text = "File Explorer",
-            highlight = "Directory",
-            text_align = "left",
-          }
-        },
-        diagnostics = "nvim_lsp",
-        diagnostics_indicator = function(count, level)
-          if level:match("error") then
-            local icon = " "
-            return " " .. icon .. count
-          else
-            return ""
-          end
-        end,
-        close_command = "Bdelete! %d",
-        right_mouse_command = "Bdelete! %d",
-        left_mouse_command = "buffer %d",
-        show_close_icon = false,
-        always_show_bufferline = false,
-        themable = true,
-      },
-    })
-
-  end
+	"Taaqif/bufferline.nvim",
+	event = "BufWinEnter",
+	config = function()
+		require("bufferline").setup({
+			options = {
+				-- separator_style = { "|", "|" },
+				separator_style = "slope",
+				indicator = {
+					style = "none",
+				},
+				offsets = {
+					{ filetype = "NvimTree", text = "File Explorer" },
+					{
+						filetype = "neo-tree",
+						text = "File Explorer",
+						highlight = "Directory",
+						text_align = "left",
+					},
+				},
+				diagnostics = "nvim_lsp",
+				diagnostics_indicator = function(count, level)
+					if level:match("error") then
+						local icon = " "
+						return " " .. icon .. count
+					else
+						return ""
+					end
+				end,
+				close_command = "Bdelete! %d",
+				right_mouse_command = "Bdelete! %d",
+				left_mouse_command = "buffer %d",
+				show_close_icon = false,
+				always_show_bufferline = false,
+				hover = {
+					enabled = true,
+					delay = 200,
+					reveal = { "close" },
+				},
+			},
+		})
+	end,
 }
