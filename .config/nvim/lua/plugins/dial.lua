cd61eb6dac50146154fa7e7f35c3a918158e225f
Tye Peck <tye@novatasolutions.com.au>
2023-01-17 17:39:56 +0800

    fix(nvim): fix dial keymaps

--- .config/nvim/lua/plugins/dial.lua
+++ .config/nvim/lua/plugins/dial.lua
@@ -2,27 +2,13 @@ return {
 	"monaqa/dial.nvim",
 	init = function()
 		local map = require("utils").keymap
-
-		map("n", "<C-a>", function()
-			require("dial.map").inc_normal()
-		end, "Increment")
-		map("n", "<C-x>", function()
-			require("dial.map").dec_normal()
-		end, "Decrement")
-		map("v", "<C-a>", function()
-			require("dial.map").inc_visual()
-		end, "Increment")
-		map("v", "<C-x>", function()
-			require("dial.map").dec_visual()
-		end, "Decrement")
-		map("v", "g<C-a>", function()
-			require("dial.map").inc_gvisual()
-		end, "Increment")
-		map("v", "g<C-x>", function()
-			require("dial.map").dec_gvisual()
-		end, "Decrement")
+		vim.api.nvim_set_keymap("n", "<C-a>", require("dial.map").inc_normal(), { noremap = true })
+		vim.api.nvim_set_keymap("n", "<C-x>", require("dial.map").dec_normal(), { noremap = true })
+		vim.api.nvim_set_keymap("v", "<C-a>", require("dial.map").inc_visual(), { noremap = true })
+		vim.api.nvim_set_keymap("v", "<C-x>", require("dial.map").dec_visual(), { noremap = true })
+		vim.api.nvim_set_keymap("v", "g<C-a>", require("dial.map").inc_gvisual(), { noremap = true })
+		vim.api.nvim_set_keymap("v", "g<C-x>", require("dial.map").dec_gvisual(), { noremap = true })
 	end,
-	event = "BufRead",
 	config = function()
 		local augend = require("dial.augend")
 		require("dial.config").augends:register_group({
