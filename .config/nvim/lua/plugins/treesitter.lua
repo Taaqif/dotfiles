ac6385ccaf02d5358ef6f1573c88508d1ec1117d
Tye Peck <tye@novatasolutions.com.au>
2023-02-10 15:45:05 +0800

    feat(nvim): set treesitter configs, textsubjects, text objects

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,47 +1,71 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = "BufRead",
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = "BufRead",
 
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    "RRethy/nvim-treesitter-textsubjects",
-    "nvim-treesitter/nvim-treesitter-refactor",
-    "mfussenegger/nvim-treehopper",
-    "JoosepAlviste/nvim-ts-context-commentstring",
-    "mrjones2014/nvim-ts-rainbow",
-    "windwp/nvim-ts-autotag",
-    "nvim-treesitter/nvim-treesitter-context",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		"RRethy/nvim-treesitter-textsubjects",
+		"nvim-treesitter/nvim-treesitter-refactor",
+		"mfussenegger/nvim-treehopper",
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		"mrjones2014/nvim-ts-rainbow",
+		"windwp/nvim-ts-autotag",
+		"nvim-treesitter/nvim-treesitter-context",
 
-    { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
-  },
+		{ "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
+	},
 
-  config = function()
-    local treesitter_context_ok, treesitter_context = pcall(require, "treesitter-context")
+	config = function()
+		local treesitter_context_ok, treesitter_context = pcall(require, "treesitter-context")
 
-    require("nvim-treesitter.configs").setup({
-      highlight = {
-        enable = true,
-      },
-      indent = { enable = true },
-      autopairs = { enable = true },
-      autotag = {
-        enable = true,
-      },
-      matchup = {
-        enable = true,
-      },
-      context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-      },
-      rainbow = {
-        enable = true,
-      },
+		require("nvim-treesitter.configs").setup({
+			highlight = {
+				enable = true,
+			},
+			indent = { enable = true },
+			autopairs = { enable = true },
+			autotag = {
+				enable = true,
+			},
+			matchup = {
+				enable = true,
+			},
+			context_commentstring = {
+				enable = true,
+				enable_autocmd = false,
+			},
+			rainbow = {
+				enable = true,
+			},
+			textsubjects = {
+				enable = true,
+				prev_selection = ",", -- (Optional) keymap to select the previous selection
+				keymaps = {
+					["."] = "textsubjects-smart",
+					[";"] = "textsubjects-container-outer",
+					["i;"] = "textsubjects-container-inner",
+				},
+			},
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+					},
+					selection_modes = {
+						["@parameter.outer"] = "v", -- charwise
+						["@function.outer"] = "V", -- linewise
+						["@class.outer"] = "<c-v>", -- blockwise
+					},
+				},
+			},
+		})
 
-    })
-
-    require 'nvim-treesitter.install'.compilers = { vim.fn.getenv('CC'), "cc", "zig", "gcc", "clang", "cl" }
-
-  end
+		require("nvim-treesitter.install").compilers = { vim.fn.getenv("CC"), "cc", "zig", "gcc", "clang", "cl" }
+	end,
 }
