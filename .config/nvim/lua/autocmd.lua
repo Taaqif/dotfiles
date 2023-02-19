d0d00c48f28333070d2e40f0258ddad1f80e70dd
Tye Peck <tye@novatasolutions.com.au>
2023-02-19 09:45:36 +0800

    feat(nvim): omit osc7 on dir change event

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,55 +1,57 @@
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = {
-    "qf",
-    "help",
-    "man",
-    "lspinfo",
-    "spectre_panel",
-    "lir",
-    "DressingSelect",
-    "tsplayground",
-    "Markdown",
-  },
-  callback = function()
-    vim.cmd [[
+	pattern = {
+		"qf",
+		"help",
+		"man",
+		"lspinfo",
+		"spectre_panel",
+		"lir",
+		"DressingSelect",
+		"tsplayground",
+		"Markdown",
+	},
+	callback = function()
+		vim.cmd([[
       nnoremap <silent> <buffer> q :close<CR> 
       nnoremap <silent> <buffer> <esc> :close<CR> 
       set nobuflisted 
-    ]]
-  end,
+    ]])
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  pattern = { "" },
-  callback = function()
-    local buf_ft = vim.bo.filetype
-    if buf_ft == "" or buf_ft == nil then
-      vim.cmd [[
+	pattern = { "" },
+	callback = function()
+		local buf_ft = vim.bo.filetype
+		if buf_ft == "" or buf_ft == nil then
+			vim.cmd([[
       nnoremap <silent> <buffer> q :close<CR> 
       nnoremap <silent> <buffer> <c-j> j<CR> 
       nnoremap <silent> <buffer> <c-k> k<CR> 
       set nobuflisted 
-    ]]
-    end
-  end,
+    ]])
+		end
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-  callback = function()
-    vim.cmd("set formatoptions-=cro")
-  end,
+	callback = function()
+		vim.cmd("set formatoptions-=cro")
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-  pattern = "*",
-  callback = function()
-    vim.highlight.on_yank { higroup = "Visual", timeout = 50 }
-  end,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Visual", timeout = 50 })
+	end,
 })
 
 -- highlight non ascii characters
 vim.api.nvim_create_autocmd("BufReadPost", {
-  pattern = "*",
-  command = [[syntax match NonAscii "[^\u0000-\u007F]"]],
+	pattern = "*",
+	command = [[syntax match NonAscii "[^\u0000-\u007F]"]],
 })
 
+--Omit OSC7 cwd change on dir change
+vim.cmd([[autocmd DirChanged * call chansend(v:stderr, printf("\033]7;file://%s\033\\", v:event.cwd))]])
