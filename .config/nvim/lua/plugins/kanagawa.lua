faae0127dde7c9e59ff283cb873ec92d7cc10962
Tye Peck <tye@novatasolutions.com.au>
2023-03-17 09:26:08 +0800

    feat(nvim): update kanagawa theme

--- .config/nvim/lua/plugins/kanagawa.lua
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -1,86 +1,113 @@
 local colors = require("colors")
+
+local function hexToRgb(c)
+	c = string.lower(c)
+	return { tonumber(c:sub(2, 3), 16), tonumber(c:sub(4, 5), 16), tonumber(c:sub(6, 7), 16) }
+end
+
+local blend = function(foreground, background, alpha)
+	alpha = type(alpha) == "string" and (tonumber(alpha, 16) / 0xff) or alpha
+	local bg = hexToRgb(background)
+	local fg = hexToRgb(foreground)
+
+	local blendChannel = function(i)
+		local ret = (alpha * fg[i] + ((1 - alpha) * bg[i]))
+		return math.floor(math.min(math.max(0, ret), 255) + 0.5)
+	end
+
+	return string.format("#%02x%02x%02x", blendChannel(1), blendChannel(2), blendChannel(3))
+end
+
 return {
 	"rebelot/kanagawa.nvim",
+	enabled = true,
 	config = function()
 		vim.o.background = "dark"
-		local c = require("kanagawa.color")
-		local default_colors = require("kanagawa.colors").setup()
 
-		local overrides = {
-			-- Nvim-Navic
-			NavicIconsFile = { fg = default_colors.springViolet2 },
-			NavicIconsModule = { fg = default_colors.boatYellow2 },
-			NavicIconsNamespace = { fg = default_colors.springViolet2 },
-			NavicIconsPackage = { fg = default_colors.springViolet1 },
-			NavicIconsClass = { fg = default_colors.surimiOrange },
-			NavicIconsMethod = { fg = default_colors.crystalBlue },
-			NavicIconsProperty = { fg = default_colors.waveAqua2 },
-			NavicIconsField = { fg = default_colors.waveAqua1 },
-			NavicIconsConstructor = { fg = default_colors.surimiOrange },
-			NavicIconsEnum = { fg = default_colors.boatYellow2 },
-			NavicIconsInterface = { fg = default_colors.carpYellow },
-			NavicIconsFunction = { fg = default_colors.crystalBlue },
-			NavicIconsVariable = { fg = default_colors.oniViolet },
-			NavicIconsConstant = { fg = default_colors.oniViolet },
-			NavicIconsString = { fg = default_colors.springGreen },
-			NavicIconsNumber = { fg = default_colors.sakuraPink },
-			NavicIconsBoolean = { fg = default_colors.surimiOrange },
-			NavicIconsArray = { fg = default_colors.waveAqua2 },
-			NavicIconsObject = { fg = default_colors.surimiOrange },
-			NavicIconsKey = { fg = default_colors.oniViolet },
-			NavicIconsNull = { fg = default_colors.carpYellow },
-			NavicIconsEnumMember = { fg = default_colors.carpYellow },
-			NavicIconsStruct = { fg = default_colors.surimiOrange },
-			NavicIconsEvent = { fg = default_colors.surimiOrange },
-			NavicIconsOperator = { fg = default_colors.springViolet2 },
-			NavicIconsTypeParameter = { fg = default_colors.springBlue },
-			NavicText = { fg = default_colors.fujiWhite },
-			NavicSeparator = { fg = default_colors.sumiInk4 },
+		local overrides = function(colors)
+			local palette = colors.palette
+			local theme = colors.theme
+			return {
+				TelescopeResultsTitle = { fg = theme.ui.bg_dim, bg = theme.ui.bg_dim },
+				TelescopePreviewTitle = { fg = theme.ui.bg_dim, bg = theme.ui.bg_dim },
+				TelescopeTitle = { bg = theme.syn.special2, fg = theme.ui.bg_dim },
+				TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+				TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+				TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_dim },
+				TelescopeResultsBorder = { fg = theme.ui.bg_dim, bg = theme.ui.bg_dim },
+				TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+				TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
 
-			-- Telescope
-			TelescopeBorder = { fg = default_colors.sumiInk0, bg = default_colors.sumiInk0 },
-			TelescopePromptBorder = { fg = default_colors.sumiInk2, bg = default_colors.sumiInk2 },
-			TelescopeResultsBorder = { fg = default_colors.sumiInk0, bg = default_colors.sumiInk0 },
-			TelescopePreviewBorder = { fg = default_colors.sumiInk0, bg = default_colors.sumiInk0 },
-			TelescopePromptNormal = { fg = colors.white, bg = default_colors.sumiInk2 },
-			TelescopePromptPrefix = { fg = default_colors.waveRed, bg = default_colors.sumiInk2 },
-			TelescopeNormal = { bg = default_colors.sumiInk0 },
-			TelescopePreviewTitle = { fg = colors.sumiInk0 },
-			TelescopePromptTitle = { fg = colors.black, bg = default_colors.waveRed },
-			TelescopeResultsTitle = { fg = default_colors.sumiInk0, bg = default_colors.sumiInk0 },
-			TelescopeSelection = { bg = default_colors.sumiInk2 },
+				Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_dim },
+				PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+				PmenuSbar = { bg = theme.ui.bg_m1 },
+				PmenuThumb = { bg = theme.ui.bg_p2 },
 
-			--Neotree
-			NeoTreeFloatTitle = { fg = colors.black, bg = default_colors.waveRed },
-			NeoTreeFloatBorder = { fg = default_colors.sumiInk0, bg = default_colors.sumiInk0 },
-			NeoTreeNormal = { bg = default_colors.sumiInk0 },
-			NeoTreeFloatNormal = { bg = default_colors.sumiInk1 },
-			NeoTreeNormalNC = { bg = default_colors.sumiInk0 },
+				NeoTreeFloatTitle = { bg = theme.syn.special2, fg = theme.ui.bg_dim },
+				NeoTreeFloatBorder = { fg = theme.ui.bg_dim, bg = theme.ui.bg_dim },
+				NeoTreeNormal = { bg = theme.ui.bg_dim },
+				NeoTreeFloatNormal = { bg = theme.ui.bg_dim },
+				NeoTreeNormalNC = { bg = theme.ui.bg_dim },
 
-			--TSRainbow
-			rainbowcol1 = { fg = default_colors.waveRed },
+				FloatTitle = { bg = theme.syn.special2, fg = theme.ui.bg_dim },
+				FloatBorder = { fg = theme.ui.bg_dim, bg = theme.ui.bg_dim },
+				NormalFloat = { bg = theme.ui.bg_dim, blend = 0 },
 
-			-- Diagnostics
-			-- DiagnosticError = { bg = tostring(c(default_colors.samuraiRed):lighten(0.1)) },
-			-- DiagnosticWarn = { bg = default_colors.roninYellow},
-			-- DiagnosticInfo = { bg = default_colors.dragonBlue },
-			-- DiagnosticHint = { bg = default_colors.waveAqua1 },
+				-- Nvim-Navic
+				NavicIconsFile = { fg = palette.springViolet2 },
+				NavicIconsModule = { fg = palette.boatYellow2 },
+				NavicIconsNamespace = { fg = palette.springViolet2 },
+				NavicIconsPackage = { fg = palette.springViolet1 },
+				NavicIconsClass = { fg = palette.surimiOrange },
+				NavicIconsMethod = { fg = palette.crystalBlue },
+				NavicIconsProperty = { fg = palette.waveAqua2 },
+				NavicIconsField = { fg = palette.waveAqua1 },
+				NavicIconsConstructor = { fg = palette.surimiOrange },
+				NavicIconsEnum = { fg = palette.boatYellow2 },
+				NavicIconsInterface = { fg = palette.carpYellow },
+				NavicIconsFunction = { fg = palette.crystalBlue },
+				NavicIconsVariable = { fg = palette.oniViolet },
+				NavicIconsConstant = { fg = palette.oniViolet },
+				NavicIconsString = { fg = palette.springGreen },
+				NavicIconsNumber = { fg = palette.sakuraPink },
+				NavicIconsBoolean = { fg = palette.surimiOrange },
+				NavicIconsArray = { fg = palette.waveAqua2 },
+				NavicIconsObject = { fg = palette.surimiOrange },
+				NavicIconsKey = { fg = palette.oniViolet },
+				NavicIconsNull = { fg = palette.carpYellow },
+				NavicIconsEnumMember = { fg = palette.carpYellow },
+				NavicIconsStruct = { fg = palette.surimiOrange },
+				NavicIconsEvent = { fg = palette.surimiOrange },
+				NavicIconsOperator = { fg = palette.springViolet2 },
+				NavicIconsTypeParameter = { fg = palette.springBlue },
+				NavicText = { fg = palette.fujiWhite },
+				NavicSeparator = { fg = palette.sumiInk4 },
 
-			-- Float
-			FloatTitle = { fg = colors.black, bg = default_colors.waveRed },
-			FloatBorder = { fg = default_colors.sumiInk0, bg = default_colors.sumiInk0 },
-			NormalFloat = { bg = default_colors.sumiInk0 },
+				DiagnosticVirtualTextError = { bg = blend(theme.diag.error, theme.ui.bg, 0.1), fg = theme.diag.error },
+				DiagnosticVirtualTextWarn = { bg = blend(theme.diag.warning, theme.ui.bg, 0.1), fg = theme.diag.warning },
+				DiagnosticVirtualTextInfo = { bg = blend(theme.diag.info, theme.ui.bg, 0.1), fg = theme.diag.info },
+				DiagnosticVirtualTextHint = { bg = blend(theme.diag.hint, theme.ui.bg, 0.1), fg = theme.diag.hint },
 
-			-- Which key
-			WhichKeyFloat = { fg = colors.white, bg = default_colors.sumiInk2 },
-
-			-- Dim Inactive
-			-- Normal = { bg = default_colors.sumiInk1b },
-			-- NormalNC = { bg = default_colors.sumiInk3 },
-		}
+				DiagnosticUnderlineError = { undercurl = true, sp = theme.diag.error }, 
+				DiagnosticUnderlineWarn = { undercurl = true, sp = theme.diag.warning },
+				DiagnosticUnderlineInfo = { undercurl = true, sp = theme.diag.info },
+				DiagnosticUnderlineHint = { undercurl = true, sp = theme.diag.hint },
+			}
+		end
 		require("kanagawa").setup({
 			transparent = false,
+			dimInactive = false,
 			overrides = overrides,
+			undercurl = false,
+			colors = {
+				theme = {
+					all = {
+						ui = {
+							bg_gutter = "none",
+						},
+					},
+				},
+			},
 		})
 		vim.cmd.colorscheme("kanagawa")
 	end,
