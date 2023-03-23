bc988dc1ae0644f85b2be9ca081c0cb8bdaa3940
Tye Peck <tye@novatasolutions.com.au>
2023-03-23 09:52:19 +0800

    feat(mvim): update kanagawa theme

--- .config/nvim/lua/plugins/kanagawa.lua
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -38,10 +38,10 @@ return {
 				TelescopePreviewNormal = { bg = theme.ui.bg_dim },
 				TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
 
-				Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_dim },
-				PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-				PmenuSbar = { bg = theme.ui.bg_m1 },
-				PmenuThumb = { bg = theme.ui.bg_p2 },
+				-- Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_dim },
+				-- PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+				-- PmenuSbar = { bg = theme.ui.bg_m1 },
+				-- PmenuThumb = { bg = theme.ui.bg_p2 },
 
 				NeoTreeFloatTitle = { bg = theme.syn.special2, fg = theme.ui.bg_dim },
 				NeoTreeFloatBorder = { fg = theme.ui.bg_dim, bg = theme.ui.bg_dim },
