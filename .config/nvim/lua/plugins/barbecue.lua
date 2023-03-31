e827352eb01d6dec4a7db5a3ee4150a9dc3f14cf
Tye Peck <tye@novatasolutions.com.au>
2023-03-31 09:18:09 +0800

    ui(nvim): ui update kind icons

--- .config/nvim/lua/plugins/barbecue.lua
+++ .config/nvim/lua/plugins/barbecue.lua
@@ -8,10 +8,12 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
+		local icons = require("icons")
 		require("barbecue").setup({
 			create_autocmd = false, -- prevent barbecue from updating itself automatically
 			show_modified = true,
 			attach_navic = false,
+			kinds = icons.kind
 		})
 
 		vim.api.nvim_create_autocmd({
