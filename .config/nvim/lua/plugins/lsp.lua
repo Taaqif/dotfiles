c7ddc9c829ec9532cf112c6e1eeac70d4ac65ece
Tye Peck <tye@novatasolutions.com.au>
2024-01-02 14:06:57 +0800

    feat(nvim): hide inlay hints by default

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,7 +3,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      inlay_hints = { enabled = true },
+      inlay_hints = { enabled = false },
       capabilities = {
         workspace = {
           didChangeWatchedFiles = {
