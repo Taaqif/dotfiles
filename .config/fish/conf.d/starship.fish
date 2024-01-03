3931313fa2ffda1ab317257020300ce2830ee54e
Tye Peck <tye@novatasolutions.com.au>
2024-01-03 09:15:13 +0800

    feat(fish): set os_icon OSC1337 variable

--- .config/fish/conf.d/starship.fish
+++ .config/fish/conf.d/starship.fish
@@ -2,52 +2,56 @@
 # find out which distribution we are running on
 set -l _distro (awk '/^ID=/' /etc/""*-release | awk -F'=' '{ print tolower($2) }')
 
- # set an icon based on the distro
+# set an icon based on the distro
 switch $_distro
-  case kali 
-    set -Ux STARSHIP_DISTRO "ﴣ"
-  case arch 
-    set -Ux STARSHIP_DISTRO ""
-  case debian 
-    set -Ux STARSHIP_DISTRO ""
-  case raspbian 
-    set -Ux STARSHIP_DISTRO ""
-  case ubuntu 
-    set -Ux STARSHIP_DISTRO ""
-  case elementary 
-    set -Ux STARSHIP_DISTRO ""
-  case fedora 
-    set -Ux STARSHIP_DISTRO ""
-  case coreos 
-    set -Ux STARSHIP_DISTRO ""
-  case gentoo 
-    set -Ux STARSHIP_DISTRO ""
-  case mageia 
-    set -Ux STARSHIP_DISTRO ""
-  case centos 
-    set -Ux STARSHIP_DISTRO ""
-  case opensuse 
-    set -Ux STARSHIP_DISTRO ""
-  case sabayon 
-    set -Ux STARSHIP_DISTRO ""
-  case slackware 
-    set -Ux STARSHIP_DISTRO ""
-  case linuxmint 
-    set -Ux STARSHIP_DISTRO ""
-  case alpine 
-    set -Ux STARSHIP_DISTRO ""
-  case aosc 
-    set -Ux STARSHIP_DISTRO ""
-  case nixos 
-    set -Ux STARSHIP_DISTRO ""
-  case devuan 
-    set -Ux STARSHIP_DISTRO ""
-  case manjaro 
-    set -Ux STARSHIP_DISTRO ""
-  case rhel 
-    set -Ux STARSHIP_DISTRO ""
-  case *
-    set -Ux STARSHIP_DISTRO ""
+    case kali
+        set -Ux STARSHIP_DISTRO ﴣ
+    case arch
+        set -Ux STARSHIP_DISTRO ""
+    case debian
+        set -Ux STARSHIP_DISTRO ""
+    case raspbian
+        set -Ux STARSHIP_DISTRO ""
+    case ubuntu
+        set -Ux STARSHIP_DISTRO ""
+    case elementary
+        set -Ux STARSHIP_DISTRO ""
+    case fedora
+        set -Ux STARSHIP_DISTRO ""
+    case coreos
+        set -Ux STARSHIP_DISTRO ""
+    case gentoo
+        set -Ux STARSHIP_DISTRO ""
+    case mageia
+        set -Ux STARSHIP_DISTRO ""
+    case centos
+        set -Ux STARSHIP_DISTRO ""
+    case opensuse
+        set -Ux STARSHIP_DISTRO ""
+    case sabayon
+        set -Ux STARSHIP_DISTRO ""
+    case slackware
+        set -Ux STARSHIP_DISTRO ""
+    case linuxmint
+        set -Ux STARSHIP_DISTRO ""
+    case alpine
+        set -Ux STARSHIP_DISTRO ""
+    case aosc
+        set -Ux STARSHIP_DISTRO ""
+    case nixos
+        set -Ux STARSHIP_DISTRO ""
+    case devuan
+        set -Ux STARSHIP_DISTRO ""
+    case manjaro
+        set -Ux STARSHIP_DISTRO ""
+    case rhel
+        set -Ux STARSHIP_DISTRO ""
+    case *
+        set -Ux STARSHIP_DISTRO ""
 end
-  
+
+function custom_precmd --on-event fish_prompt
+    printf "\033]1337;SetUserVar=os_icon=%s\007" (echo -n $STARSHIP_DISTRO | base64)
+end
+
 starship init fish | source
