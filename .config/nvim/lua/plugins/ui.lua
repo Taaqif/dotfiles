d93e28a76021eb1a1991df3a041d2a9e866cf3e8
Tye Peck <tye@novatasolutions.com.au>
2024-01-22 12:20:50 +0800

    ui(nvim): update lualine with slant

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -107,6 +107,8 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
+      opts.options.component_separators = { left = "", right = "" }
+      opts.options.section_separators = { left = "", right = "" }
       opts.sections.lualine_z = {
         {
           wakatime,
