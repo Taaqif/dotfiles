fea7ff518192dbd37b12ca53f530cbee5ed80c2e
Tye Peck <tye@novatasolutions.com.au>
2024-12-09 14:20:37 +0800

    feat(nvim): better keymaps, c-v and c,d

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
 -- Don't copy the replaced text after pasting in visual mode
 map("v", "p", "P")
+
+-- make c-v paste instead
+map("i", "<C-v>", "<C-r>+", opts)
+
+-- Use the black hole register for the c, d command
+map({ "n", "x" }, "c", '"_c', opts)
+map({ "n", "x" }, "C", '"_C', opts)
+map({ "n", "x" }, "d", '"_d', opts)
+map({ "n", "x" }, "D", '"_D', opts)
