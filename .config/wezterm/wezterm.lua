32cf1c1c453a0a6f941d549564ac802dadeb8614
Tye Peck <tye@novatasolutions.com.au>
2025-04-30 14:33:41 +0800

    chore(wezterm): migrate to wezterm config builder

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,138 +2,138 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 local utils = require("utils")
 local colors = require("colors")
-local color_key = "kanagawa"
-COLOR = colors[color_key]
-COLOR_CUSTOM = colors[color_key .. "_custom"]
-
 local keybinds = require("keybinds")
 require("on")
 
-local launch_menu = {}
+local config = wezterm.config_builder()
+
+local color_key = "kanagawa"
+COLOR = colors[color_key]
+COLOR_CUSTOM = colors[color_key .. "_custom"]
 
 local font_with_fallback = function(name, params)
 	local names = { name, "JetBrains Mono" }
 	return wezterm.font_with_fallback(names, params)
 end
 
-return {
-	font_size = 11.0,
-	check_for_updates = true,
-	term = "xterm-256color",
-	tab_bar_at_bottom = true,
-	tab_bar_style = {
-		window_maximize_hover = wezterm.format({
-			{ Background = { Color = COLOR.tab_bar.active_tab.fg_color } },
-			{ Foreground = { Color = COLOR.tab_bar.active_tab.bg_color } },
-			{ Text = " " .. utils.icons.MAXIMIZE .. " " },
-		}),
-		window_close_hover = wezterm.format({
-			{ Background = { Color = COLOR.tab_bar.active_tab.fg_color } },
-			{ Foreground = { Color = COLOR.tab_bar.active_tab.bg_color } },
-			{ Text = " " .. utils.icons.CLOSE .. " " },
-		}),
-		window_hide_hover = wezterm.format({
-			{ Background = { Color = COLOR.tab_bar.active_tab.fg_color } },
-			{ Foreground = { Color = COLOR.tab_bar.active_tab.bg_color } },
-			{ Text = " " .. utils.icons.HIDE .. " " },
-		}),
-		window_maximize = wezterm.format({
-			{ Background = { Color = COLOR.tab_bar.active_tab.bg_color } },
-			{ Foreground = { Color = COLOR.tab_bar.active_tab.fg_color } },
-			{ Text = " " .. utils.icons.MAXIMIZE .. " " },
-		}),
-		window_close = wezterm.format({
-			{ Background = { Color = COLOR.tab_bar.active_tab.bg_color } },
-			{ Foreground = { Color = COLOR.tab_bar.active_tab.fg_color } },
-			{ Text = " " .. utils.icons.CLOSE .. " " },
-		}),
-		window_hide = wezterm.format({
-			{ Background = { Color = COLOR.tab_bar.active_tab.bg_color } },
-			{ Foreground = { Color = COLOR.tab_bar.active_tab.fg_color } },
-			{ Text = " " .. utils.icons.HIDE .. " " },
-		}),
-		new_tab = wezterm.format({
-			{ Text = "  " .. utils.icons.NEW .. " " },
-		}),
-		new_tab_hover = wezterm.format({
-			{ Background = { Color = "NONE" } },
-			{ Foreground = { Color = COLOR.tab_bar.active_tab.bg_color } },
-			{ Text = "  " .. utils.icons.NEW .. "  " },
-		}),
-	},
-	line_height = 1,
-	-- default_domain = 'WSL:Ubuntu',
-	default_prog = { "pwsh" },
-	prefer_to_spawn_tabs = true,
-	use_ime = true,
-	initial_rows = 32,
-	initial_cols = 120,
-	font = font_with_fallback("Operator Mono SSm Lig Book", { style = "Normal" }),
-	font_rules = {
-		{
-			italic = false,
-			intensity = "Normal",
-			font = font_with_fallback("Operator Mono SSm Lig Book", { style = "Normal" }),
-		},
-		{
-			italic = true,
-			intensity = "Normal",
-			font = font_with_fallback("Operator Mono SSm Lig Book", { style = "Italic" }),
-		},
+config.font_size = 11.0
+config.check_for_updates = true
+config.term = "xterm-256color"
+config.tab_bar_at_bottom = true
+config.tab_bar_style = {
+	window_maximize_hover = wezterm.format({
+		{ Background = { Color = COLOR.tab_bar.active_tab.fg_color } },
+		{ Foreground = { Color = COLOR.tab_bar.active_tab.bg_color } },
+		{ Text = " " .. utils.icons.MAXIMIZE .. " " },
+	}),
+	window_close_hover = wezterm.format({
+		{ Background = { Color = COLOR.tab_bar.active_tab.fg_color } },
+		{ Foreground = { Color = COLOR.tab_bar.active_tab.bg_color } },
+		{ Text = " " .. utils.icons.CLOSE .. " " },
+	}),
+	window_hide_hover = wezterm.format({
+		{ Background = { Color = COLOR.tab_bar.active_tab.fg_color } },
+		{ Foreground = { Color = COLOR.tab_bar.active_tab.bg_color } },
+		{ Text = " " .. utils.icons.HIDE .. " " },
+	}),
+	window_maximize = wezterm.format({
+		{ Background = { Color = COLOR.tab_bar.active_tab.bg_color } },
+		{ Foreground = { Color = COLOR.tab_bar.active_tab.fg_color } },
+		{ Text = " " .. utils.icons.MAXIMIZE .. " " },
+	}),
+	window_close = wezterm.format({
+		{ Background = { Color = COLOR.tab_bar.active_tab.bg_color } },
+		{ Foreground = { Color = COLOR.tab_bar.active_tab.fg_color } },
+		{ Text = " " .. utils.icons.CLOSE .. " " },
+	}),
+	window_hide = wezterm.format({
+		{ Background = { Color = COLOR.tab_bar.active_tab.bg_color } },
+		{ Foreground = { Color = COLOR.tab_bar.active_tab.fg_color } },
+		{ Text = " " .. utils.icons.HIDE .. " " },
+	}),
+	new_tab = wezterm.format({
+		{ Text = "  " .. utils.icons.NEW .. " " },
+	}),
+	new_tab_hover = wezterm.format({
+		{ Background = { Color = "NONE" } },
+		{ Foreground = { Color = COLOR.tab_bar.active_tab.bg_color } },
+		{ Text = "  " .. utils.icons.NEW .. "  " },
+	}),
+}
+config.line_height = 1
+config.default_prog = { "pwsh" }
+config.prefer_to_spawn_tabs = true
+config.use_ime = true
+config.initial_rows = 32
+config.initial_cols = 120
+config.font = font_with_fallback("Operator Mono SSm Lig Book", { style = "Normal" })
 
-		{
-			italic = false,
-			intensity = "Bold",
-			font = font_with_fallback("Operator Mono SSm Lig Bold", { style = "Normal" }),
-		},
-		{
-			italic = true,
-			intensity = "Bold",
-			font = font_with_fallback("Operator Mono SSm Lig Bold", { style = "Italic" }),
-		},
-		{
-			italic = false,
-			intensity = "Half",
-			font = font_with_fallback("Operator Mono SSm Lig Medium", { style = "Normal" }),
-		},
-		{
-			italic = true,
-			intensity = "Half",
-			font = font_with_fallback("Operator Mono SSm Lig Medium", { style = "Italic" }),
-		},
-	},
-	window_padding = {
-		left = 0,
-		right = 0,
-		top = 2,
-		bottom = 0,
-	},
-	launch_menu = launch_menu,
-	disable_default_key_bindings = true,
-	leader = { key = " ", mods = "CTRL", timeout_milliseconds = 1000 },
-
-	keys = keybinds.keybinds,
-	key_tables = keybinds.key_tables,
-	mouse_bindings = keybinds.mouse_bindings,
-
-	visual_bell = {
-		fade_in_duration_ms = 75,
-		fade_out_duration_ms = 75,
-		target = "CursorColor",
-	},
-	use_fancy_tab_bar = false,
-	window_frame = {
+config.font_rules = {
+	{
+		italic = false,
+		intensity = "Normal",
 		font = font_with_fallback("Operator Mono SSm Lig Book", { style = "Normal" }),
-		font_size = 9.0,
 	},
-	-- enable_scroll_bar = true,
-	-- window_background_opacity = 0.93,
-	window_decorations = "INTEGRATED_BUTTONS|RESIZE",
-	window_background_opacity = 0,
-	win32_system_backdrop = "Mica",
-	force_reverse_video_cursor = true,
-	colors = COLOR,
-	set_environment_variables = {
-		VTE_VERSION = "6003",
+	{
+		italic = true,
+		intensity = "Normal",
+		font = font_with_fallback("Operator Mono SSm Lig Book", { style = "Italic" }),
+	},
+	{
+		italic = false,
+		intensity = "Bold",
+		font = font_with_fallback("Operator Mono SSm Lig Bold", { style = "Normal" }),
+	},
+	{
+		italic = true,
+		intensity = "Bold",
+		font = font_with_fallback("Operator Mono SSm Lig Bold", { style = "Italic" }),
+	},
+	{
+		italic = false,
+		intensity = "Half",
+		font = font_with_fallback("Operator Mono SSm Lig Medium", { style = "Normal" }),
+	},
+	{
+		italic = true,
+		intensity = "Half",
+		font = font_with_fallback("Operator Mono SSm Lig Medium", { style = "Italic" }),
 	},
 }
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 2,
+	bottom = 0,
+}
+
+config.launch_menu = {}
+config.disable_default_key_bindings = true
+config.leader = { key = " ", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = keybinds.keybinds
+config.key_tables = keybinds.key_tables
+config.mouse_bindings = keybinds.mouse_bindings
+
+config.visual_bell = {
+	fade_in_duration_ms = 75,
+	fade_out_duration_ms = 75,
+	target = "CursorColor",
+}
+
+config.use_fancy_tab_bar = false
+config.window_frame = {
+	font = font_with_fallback("Operator Mono SSm Lig Book", { style = "Normal" }),
+	font_size = 9.0,
+}
+
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.window_background_opacity = 0
+config.win32_system_backdrop = "Mica"
+config.force_reverse_video_cursor = true
+config.colors = COLOR
+config.set_environment_variables = {
+	VTE_VERSION = "6003",
+}
+
+return config
