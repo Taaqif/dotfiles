c67c11316eb1389aa6ba1026733885709391e709
Tye Peck <tye@novatasolutions.com.au>
2025-05-25 23:33:20 +0800

    feat(win): update bootstrap

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -4,14 +4,41 @@ function New-Symlink {
         [string]$Target,
         [string]$ItemType
     )
-    New-Item -Path $Path -ItemType $ItemType -Value (Get-Item $Target | Resolve-Path).ProviderPath -Force
+    if (Test-Path $Path) {
+        Write-Host "Removing existing $Path"
+        Remove-Item -Path $Path -Recurse -Force
+    }
+    $ResolvedTarget = (Get-Item $Target | Resolve-Path).ProviderPath
+    Write-Host "Linking $Path -> $ResolvedTarget as $ItemType"
+    New-Item -Path $Path -ItemType $ItemType -Value $ResolvedTarget
 }
+
+# Verify Nuget version is installed
+$version = "2.8.5.208"
+Write-Host "Verifying NuGet $version or later is installed"
+$nuget = Get-PackageProvider -Name NuGet -ListAvailable -ErrorAction SilentlyContinue |
+                Sort-Object -Property {[version]$_.version} | Select-Object -Last 1
+
+if(-not $nuget -or [version]$nuget.version -lt [version]$version){
+    Write-Host "Installing NuGet $($nuget.Version)"
+    $null = Install-PackageProvider -Name NuGet -MinimumVersion $nuget.version -Force
+}
+
+# Install Required Modules
+Install-Module PSReadLine -Repository PSGallery -Scope CurrentUser -Force -Confirm:$false
+Install-Module Terminal-Icons -Repository PSGallery -Scope CurrentUser -Force -Confirm:$false
 
 # Komorebi
 New-Symlink -Path "${env:USERPROFILE}/.config/komorebi" -Target ".\.config\komorebi\" -ItemType "Junction"
 
+# Git Config
+New-Symlink -Path "${env:USERPROFILE}/.config/.gitconfig" -Target ".\.config\.gitconfig" -ItemType "HardLink"
+
 # Powershell Profile
 New-Symlink -Path "$PROFILE" -Target "./PowerShell/Microsoft.PowerShell_profile.ps1" -ItemType "HardLink"
+
+# Wakatime config
+New-Symlink -Path "${env:USERPROFILE}/.config/.wakatime.cfg" -Target ".\.config\.wakatime.cfg" -ItemType "HardLink"
 
 # Starship
 New-Symlink -Path "${env:USERPROFILE}/.config/starship.toml" -Target ".\.config\starship.toml" -ItemType "HardLink"
