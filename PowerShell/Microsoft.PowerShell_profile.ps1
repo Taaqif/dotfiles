e5707c07b8fc52d04c9290ca4b73a46d443fa108
Tye Peck <tye@novatasolutions.com.au>
2025-05-27 09:59:26 +0800

    feat(win): add yazi

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -89,6 +89,16 @@ Function Invoke-Lfcd {
 Set-Alias -Name lfcd -Value Invoke-Lfcd -Option AllScope -Scope Global -Force
 Set-Alias -Name lg -Value lazygit -Option AllScope -Scope Global -Force
 
+function y {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd))
+    }
+    Remove-Item -Path $tmp
+}
+
 # fnm 
 fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
 
